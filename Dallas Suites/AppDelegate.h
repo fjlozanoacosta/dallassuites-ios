4d3a6b43eadd9e6e99321aa2a5b9abd93de0ba33
Mike Pesate <Mike@Mikes-Mac-Pro.local>
2014-10-24 13:10:24 -0430

    Initial Commit

---
+++ Dallas Suites/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dallas Suites
+//
+//  Created by Mike Pesate on 10/24/14.
+//  Copyright (c) 2014 ICO Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
