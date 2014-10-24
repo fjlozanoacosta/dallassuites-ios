8076d619ef8a9d1d5f6c51aa04c0f7ebf2bfd671
Mike Pesate <Mike@Mikes-Mac-Pro.local>
2014-10-24 18:01:25 -0430

    :star: Se agrego Home View + PopUp (Solo Storyboard)

--- Dallas Suites/HomeViewController.m
+++ Dallas Suites/HomeViewController.m
@@ -6,13 +6,13 @@
 //  Copyright (c) 2014 ICO Group. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "HomeViewController.h"
 
-@interface ViewController ()
+@interface HomeViewController ()
 
 @end
 
-@implementation ViewController
+@implementation HomeViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
